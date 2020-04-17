0845f14b43a310eb33f568bdf85abb30ac6db494
Andrew Palmer <andrew.pa@outlook.com>
2020-04-17 11:28:11 -0700

    undo in piece table, beginnings of modes and commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,215 @@
+extern crate runic;
 
 mod piece_table;
+use runic::*;
+use piece_table::{PieceTable, PieceTableRenderer};
+use std::error::Error as ErrorTrait;
+
+#[derive(Debug)]
+enum Error {
+    Other(Box<dyn ErrorTrait + 'static>)
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "error")
+    }
+}
+
+impl ErrorTrait for Error {
+    fn source(&self)  -> Option<&(dyn ErrorTrait + 'static)> {
+        match self {
+            Error::Other(e) => Some(e.as_ref()),
+            _ => None
+        }
+    }
+}
+
+
+enum Operator {
+    Move,
+    Repeat,
+    Undo,
+    Delete,
+    Change,
+    Yank,
+    Put
+}
+
+enum Direction { Forward, Backward }
+
+enum TextObject {
+    Char,
+    Word(Direction), // words
+    BigWord(Direction), // WORDS
+    EndOfWord(Direction),
+    EndOfBigWord(Direction),
+    NextChar {
+        c: char,
+        place_before: bool,
+        direction: Direction, // true -> towards the end
+    },
+    RepeatNextChar {
+        opposite: bool // true -> reverse direction
+    },
+    Line,
+    StartOfLine,
+    EndOfLine,
+    Paragraph
+}
+
+enum TextObjectMod {
+    None, AnObject, InnerObject
+}
+
+struct Motion {
+    count: usize,
+    object: TextObject,
+    modifier: TextObjectMod
+}
+
+enum Command {
+    Edit {
+        op: Operator,
+        op_count: usize,
+        mo: Motion,
+    },
+    ChangeMode { new_mode: Box<dyn Mode> },
+}
+
+impl Command {
+    fn parse(s: &str) -> Result<Option<Command>, Error> {
+        Ok(None)
+    }
+
+    fn execute(&self, buf: &mut Buffer) -> Result<Option<Box<dyn Mode>>, Error> {
+        Ok(None)
+    }
+}
+
+trait Mode {
+    fn status_tag(&self) -> &'static str;
+    fn event(&mut self, e: runic::Event, buf: &mut Buffer) -> Result<Option<Box<dyn Mode>>, Error>;
+}
+
+struct NormalMode {
+    pending_buf: String
+}
+
+impl NormalMode {
+    fn new() -> NormalMode {
+        NormalMode { pending_buf: String::new() }
+    }
+}
+
+impl Mode for NormalMode {
+    fn status_tag(&self) -> &'static str { "normal" }
+    fn event(&mut self, e: runic::Event, buf: &mut Buffer) -> Result<Option<Box<dyn Mode>>, Error> {
+        match e {
+            Event::ReceivedCharacter(c) => {
+                self.pending_buf.push(c);
+                match Command::parse(&self.pending_buf) {
+                    Ok(Some(cmd)) => {
+                        let res = cmd.execute(buf);
+                        self.pending_buf.clear();
+                        res
+                    },
+                    Ok(None) => Ok(None),
+                    Err(e) => { 
+                        self.pending_buf.clear();
+                        Err(e)
+                    }
+                }
+                /*Ok(Some(Box::new(InsertMode {
+                    tmut: buf.text.insert_mutator(buf.cursor_index)
+                })))*/
+            },
+            _ => Ok(None)
+        }
+    }
+}
+
+struct InsertMode {
+    tmut: piece_table::TableMutator
+}
+
+impl Mode for InsertMode {
+    fn status_tag(&self) -> &'static str { "insert" }
+    fn event(&mut self, e: Event, buf: &mut Buffer) -> Result<Option<Box<dyn Mode>>, Error> {
+        match e {
+            Event::ReceivedCharacter(c) => {
+                self.tmut.push_char(&mut buf.text, c);
+                buf.cursor_index += 1;
+                Ok(None)
+            },
+            Event::KeyboardInput { input, .. } => {
+                match input.virtual_keycode {
+                    Some(VirtualKeyCode::Back) => {
+                        self.tmut.pop_char(&mut buf.text);
+                        buf.cursor_index -= 1;
+                        Ok(None)
+                    },
+                    Some(VirtualKeyCode::Escape) => Ok(Some(Box::new(NormalMode::new()))),
+                    _ => Ok(None)
+                }
+            },
+            _ => Ok(None)
+        }
+    }
+}
+
+struct Buffer {
+    text: PieceTable,
+    path: Option<std::path::PathBuf>,
+    cursor_index: usize
+}
+
+impl Default for Buffer {
+    fn default() -> Buffer {
+        Buffer {
+            text: PieceTable::default(),
+            path: None,
+            cursor_index: 0
+        }
+    }
+}
+
+struct PkApp {
+    fnt: Font, buf: Buffer, txr: PieceTableRenderer,
+    mode: Box<dyn Mode>
+}
+
+impl runic::App for PkApp {
+    fn init(rx: &mut RenderContext) -> Self {
+        let fnt = rx.new_font("Fira Code", 12.0, FontWeight::Regular, FontStyle::Normal).unwrap();
+        let txr = PieceTableRenderer::init(rx, fnt.clone());
+        PkApp {
+            fnt, txr, buf: Buffer::default(),
+            mode: Box::new(NormalMode::new())
+        }
+    }
+
+    fn event(&mut self, e: runic::Event) -> bool {
+        match e {
+            Event::CloseRequested => return true,
+            _ => {
+                if let Some(new_mode) = self.mode.event(e, &mut self.buf).unwrap() {
+                    self.mode = new_mode;
+                }
+            }
+        }
+        false
+    }
+
+    fn paint(&mut self, rx: &mut RenderContext) {
+        rx.clear(Color::black());
+        rx.set_color(Color::rgb(0.7, 0.35, 0.0));
+        rx.draw_text(Rect::xywh(4.0, 2.0, 100.0, 100.0),  self.mode.status_tag(), &self.fnt);
+        self.txr.cursor_index = self.buf.cursor_index;
+        self.txr.paint(rx, &self.buf.text, Point::xy(8.0, 18.0));
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    runic::start::<PkApp>(WindowOptions::new().with_title("pk"))
 }
--- src/piece_table.rs
+++ src/piece_table.rs
@@ -16,31 +16,50 @@ impl Piece {
 }
 
 #[derive(Debug)]
+enum Change {
+    Insert {
+        piece_index: usize,
+    },
+    Modify {
+        piece_index: usize,
+        old: Piece
+    },
+    Delete {
+        piece_index: usize,
+        old: Piece
+    }
+}
+
+type Action=Vec<Change>;
+
+#[derive(Debug)]
 pub struct PieceTable {
     sources: Vec<String>,
-    pieces: Vec<Piece>
+    pieces: Vec<Piece>,
+    history: Vec<Action>
 }
 
 impl Default for PieceTable {
     fn default() -> PieceTable {
         PieceTable {
-            sources: vec![String::new()],
-            pieces: vec![ Piece { source: 0, start: 0, length: 0 } ]
+            sources: vec![String::from("Hello, world!")],
+            pieces: vec![ Piece { source: 0, start: 0, length: 13 } ],
+            history: Vec::new()
         }
     }
 }
 
-struct TableMutator {
+pub struct TableMutator {
     piece_ix: usize
 }
 impl TableMutator {
-    fn push_char(&mut self, pt: &mut PieceTable, c: char) {
+    pub fn push_char(&mut self, pt: &mut PieceTable, c: char) {
         pt.pieces[self.piece_ix].length += 1;
         let si = pt.pieces[self.piece_ix].source;
         pt.sources[si].push(c);
     }
 
-    fn pop_char(&mut self, pt: &mut PieceTable) -> bool {
+    pub fn pop_char(&mut self, pt: &mut PieceTable) -> bool {
         if pt.pieces[self.piece_ix].length == 0 {
             return true;
         }
@@ -52,26 +71,55 @@ impl TableMutator {
 }
 
 impl<'table> PieceTable {
-    fn with_text(s: &str) -> PieceTable {
+    pub fn with_text(s: &str) -> PieceTable {
         PieceTable {
             sources: vec![s.to_string()],
-            pieces: vec![ Piece { source: 0, start: 0, length: s.len() } ]
+            pieces: vec![ Piece { source: 0, start: 0, length: s.len() } ],
+            history: Vec::new()
         }
     }
 
-    fn insert_range(&mut self, s: &str, index: usize) {
+    fn reverse_change(&mut self, change: &Change) {
+        match *change {
+            Change::Insert { piece_index } => {
+                self.pieces.remove(piece_index);
+            },
+            Change::Modify { piece_index, old } => {
+                self.pieces[piece_index] = old;
+            },
+            Change::Delete { piece_index, old } => {
+                self.pieces.insert(piece_index, old);
+            },
+        }
+    }
+
+    pub fn undo(&mut self) {
+        if let Some(action) = self.history.pop() {
+            for change in action.iter().rev() {
+                self.reverse_change(&change);
+            }
+        }
+    }
+
+    pub fn insert_range(&mut self, s: &str, index: usize) {
         let mut ix = 0usize;
         let new_piece = Piece { source: self.sources.len(), start: 0, length: s.len() };
         self.sources.push(String::from(s));
+        let mut action = Action::new();
         for (i,p) in self.pieces.iter().enumerate() {
             println!("{} {:?}", i, p);
             if index >= ix && index < ix+p.length {
                 if index == ix { // we're inserting at the start of this piece
                     self.pieces.insert(i, new_piece);
+                    action.push(Change::Insert{piece_index: i});
                 } else if index == ix+p.length { // we're inserting at the end of this piece
                     self.pieces.insert(i+1, new_piece);
+                    action.push(Change::Insert{piece_index: i+1});
                 } else { //insertion in the middle
                     let (left,right) = p.split(index-ix);
+                    action.push(Change::Modify{piece_index: i, old: p.clone()});
+                    action.push(Change::Insert{piece_index: i+1});
+                    action.push(Change::Insert{piece_index: i+2});
                     self.pieces[i] = left;
                     self.pieces.insert(i+1, new_piece);
                     self.pieces.insert(i+2, right);
@@ -80,16 +128,19 @@ impl<'table> PieceTable {
             }
             ix += p.length;
         }
+        self.history.push(action);
     }
 
-    fn insert_mutator(&mut self, index: usize) -> TableMutator {
+    pub fn insert_mutator(&mut self, index: usize) -> TableMutator {
         let mut ix = 0usize;
         let mut insertion_piece_index: Option<usize> = None;
+        let mut action = Action::new();
         for (i,p) in self.pieces.iter().enumerate() {
             if index >= ix && index < ix+p.length {
                 if index == ix { // we're inserting at the start of this piece
                     self.pieces.insert(i, Piece { source: self.sources.len(), start: 0, length: 0 });
                     self.sources.push(String::new());
+                    action.push(Change::Insert { piece_index: i });
                     insertion_piece_index = Some(i);
                 } else if index == ix+p.length-1 { // we're inserting at the end of this piece
                     if self.sources[p.source].len() ==
@@ -97,6 +148,7 @@ impl<'table> PieceTable {
                         insertion_piece_index = Some(i);
                     } else {
                         self.pieces.insert(i+1, Piece { source: self.sources.len(), start: 0, length: 0 });
+                        action.push(Change::Insert { piece_index: i+1 });
                         self.sources.push(String::new());
                         insertion_piece_index = Some(i+1);
                     }
@@ -104,6 +156,9 @@ impl<'table> PieceTable {
                     let (a,c) = p.split(index-ix);
                     let b = Piece { source: self.sources.len(), start: 0, length: 0 };
                     self.sources.push(String::new());
+                    action.push(Change::Modify { piece_index: i, old: p.clone() });
+                    action.push(Change::Insert { piece_index: i+1 });
+                    action.push(Change::Insert { piece_index: i+2 });
                     self.pieces[i] = a;
                     self.pieces.insert(i+1, b);
                     self.pieces.insert(i+2, c);
@@ -113,16 +168,19 @@ impl<'table> PieceTable {
             }
             ix += p.length;
         }
-        TableMutator { piece_ix: insertion_piece_index.unwrap() }
+        self.history.push(action);
+        let insertion_piece_index = insertion_piece_index.unwrap();
+        TableMutator { piece_ix: insertion_piece_index }
     }
 
-    fn delete_range(&mut self, start: usize, end: usize) {
+    pub fn delete_range(&mut self, start: usize, end: usize) {
         assert!(start != end);
 
         let mut start_piece: Option<(usize,usize)> = None;
         let mut end_piece:   Option<(usize,usize)> = None;
         let mut mid_pieces:  Vec<usize>            = Vec::new();
         let mut global_index                       = 0usize;
+        let mut action = Action::new();
 
         for (i,p) in self.pieces.iter().enumerate() {
             if start < global_index && end >= global_index+p.length {
@@ -131,13 +189,16 @@ impl<'table> PieceTable {
                 if end > global_index && end <= global_index+p.length {
                     // this piece totally contains this range
                     if start-global_index == 0 && end-global_index == p.length {
-                        self.pieces.remove(i);
+                        action.push(Change::Delete { piece_index: i, old: self.pieces.remove(i) });
                     } else {
                         let (left_keep, deleted_right) = p.split(start-global_index);
                         let (_deleted, right_keep) = deleted_right.split(end-(global_index+left_keep.length-1));
+                        action.push(Change::Modify { piece_index: i, old: p.clone() });
+                        action.push(Change::Insert { piece_index: i+1 });
                         self.pieces[i] = left_keep;
                         self.pieces.insert(i+1, right_keep);
                     }
+                    self.history.push(action);
                     return;
                 } else {
                     start_piece = Some((i, start-global_index));
@@ -153,15 +214,20 @@ impl<'table> PieceTable {
         let (start_piece, start_cut) = start_piece.unwrap();
         let (end_piece,   end_cut)   = end_piece.unwrap();
 
+        action.push(Change::Modify { piece_index: start_piece, old: self.pieces[start_piece] });
+        action.push(Change::Modify { piece_index: end_piece, old: self.pieces[end_piece] });
+
         self.pieces[start_piece] = self.pieces[start_piece].split(start_cut).0;
         self.pieces[end_piece]   = self.pieces[end_piece].split(end_cut).1;
 
         for i in mid_pieces {
-            self.pieces.remove(i);
+            action.push(Change::Delete { piece_index: i,  old: self.pieces.remove(i) });
         }
+
+        self.history.push(action);
     }
 
-    fn copy_range(&mut self, start: usize, end: usize) -> String {
+    pub fn copy_range(&mut self, start: usize, end: usize) -> String {
         let mut buf = String::new();
         let mut global_index = 0usize;
         for p in self.pieces.iter() {
@@ -183,13 +249,64 @@ impl<'table> PieceTable {
         buf
     }
 
-    fn text(&self) -> String {
+    pub fn text(&self) -> String {
         let len = self.pieces.iter().fold(0, |a,p| a+p.length);
         let mut s = String::with_capacity(len);
         for piece in self.pieces.iter() {
            s.push_str(&self.sources[piece.source][piece.start..(piece.start+piece.length)]); 
         }
         s
+    }
+}
+
+pub struct PieceTableRenderer {
+    fnt: Font,
+    pub cursor_index: usize
+}
+
+use runic::*;
+impl PieceTableRenderer {
+    pub fn init(rx: &mut RenderContext, fnt: Font) -> Self {
+        PieceTableRenderer { fnt, cursor_index: 0 }
+    }
+
+    pub fn paint(&mut self, rx: &mut RenderContext, table: &PieceTable, position: Point) {
+        rx.set_color(Color::white());
+        let mut global_index = 0usize;
+        let mut cur_pos = position; 
+        let mut line_num = 0usize;
+        for p in table.pieces.iter() {
+            let src = &table.sources[p.source][p.start..(p.start+p.length)];
+            let mut lni = src.lines().peekable(); 
+            loop {
+                let ln = lni.next();
+                if ln.is_none() { break; }
+                let ln = ln.unwrap();
+                let layout = rx.new_text_layout(ln, &self.fnt, 10000.0, 10000.0).expect("create text layout");
+                rx.draw_text_layout(cur_pos, &layout);
+                //rx.draw_text(Rect::pnwh(cur_pos - Point::x(12.0), 100.0, 100.0), &format!("{}", global_index), &self.fnt);
+                if self.cursor_index >= global_index && self.cursor_index <= global_index+ln.len() {
+                    //fix: the cursor doesn't appear at the end of lines because DWrite returns a
+                    // zero-area rectangle, so that case needs to be checked for and a cursor
+                    // rectangle manually computed
+                    let curbounds = layout.char_bounds(self.cursor_index - global_index);
+                    //rx.fill_rect(Rect::xywh(0.0,0.0,8.0,8.0));
+                    rx.stroke_rect(curbounds.offset(cur_pos), 2.0);
+                }
+                let text_size = layout.bounds();
+                cur_pos.x += text_size.w; 
+                global_index += ln.len();
+                if let Some(_) = lni.peek() {
+                    // new line
+                    line_num+=1;
+                    cur_pos.x = position.x;
+                    cur_pos.y += text_size.h;
+                    global_index += 1;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
 
@@ -251,4 +368,80 @@ mod test {
         assert_eq!(pt.copy_range(1,4), "elXl");
         println!("{:#?}", pt);
     }
+
+    #[test]
+    fn undo_insert_cont() {
+        let mut pt = PieceTable::with_text("hello");
+        let mut m = pt.insert_mutator(2);
+        m.push_char(&mut pt, 'A');
+        m.push_char(&mut pt, 'B');
+        assert_eq!(pt.text(), "heABllo");
+        m.pop_char(&mut pt);
+        m.push_char(&mut pt, 'C');
+        assert_eq!(pt.text(), "heACllo");
+        pt.undo();
+        assert_eq!(pt.text(), "hello");
+    }
+   
+
+    #[test]
+    fn undo_insert_range_once() {
+        let mut pt = PieceTable::with_text("hi");
+        pt.insert_range("ABCD", 1);
+        assert_eq!(pt.text(), "hABCDi");
+        pt.undo();
+        assert_eq!(pt.text(), "hi");
+    }
+    
+    #[test]
+    fn undo_insert_range_multiple() {
+        let mut pt = PieceTable::with_text("hi");
+        pt.insert_range("ABCD", 1);
+        pt.insert_range("X", 2);
+        assert_eq!(pt.text(), "hAXBCDi");
+        pt.undo();
+        assert_eq!(pt.text(), "hABCDi");
+        pt.undo();
+        assert_eq!(pt.text(), "hi");
+    }
+
+    #[test]
+    fn undo_delete_range_single_piece() {
+        let mut pt = PieceTable::with_text("hello");
+        pt.delete_range(1,3);
+        assert_eq!(pt.text(), "ho");
+        pt.undo();
+        assert_eq!(pt.text(), "hello");
+    }
+ 
+    #[test]
+    fn undo_delete_range_multiple_pieces() {
+        let mut pt = PieceTable::with_text("hello");
+        pt.insert_range("X", 3); //hel|X|lo
+        println!("{:#?}", pt);
+        pt.delete_range(1,4);
+        assert_eq!(pt.text(), "hlo");
+        pt.undo();
+        assert_eq!(pt.text(), "helXlo");
+        pt.undo();
+        assert_eq!(pt.text(), "hello");
+    }
+
+
 }
+
+struct TextRenderer {
+    fnt: Font
+}
+
+impl TextRenderer {
+    fn init(rx: &mut RenderContext, fnt: Font) -> Self {
+        TextRenderer { fnt }
+    }
+
+    fn paint(&mut self, rx: &mut RenderContext, table: &PieceTable) {
+        rx.set_color(Color::white());
+        rx.draw_text(Rect::xywh(32.0,32.0,128.0,64.0), "Hello, world! ==>", &self.fnt);
+    }
+}
+
